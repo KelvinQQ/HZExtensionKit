1bc3c1569d88f6bcd7d7df5d68111c16722f50da
HistoryZhang <history_zq@163.com>
2015-01-15 16:34:54 +0800

    保存URL和经纬度方法

--- HZExtensionKit/HZPreference/HZPreference.h
+++ HZExtensionKit/HZPreference/HZPreference.h
@@ -8,17 +8,23 @@
 
 @import Foundation;
 @import UIKit;
+@import CoreLocation;
 
 @interface HZPreference : NSObject
 + (void)saveObject:(id)object forKey:(NSString *)key;
 + (void)saveBool:(BOOL)value forKey:(NSString *)key;
 + (void)saveInteger:(NSInteger)value forKey:(NSString *)key;
 + (void)saveFloat:(CGFloat)value forKey:(NSString *)key;
++ (void)saveURL:(NSURL *)URL forKey:(NSString *)key;
++ (void)saveCoordinate:(CLLocationCoordinate2D)coordinate forKey:(NSString *)key;
 
 + (void)removeObjectForKey:(NSString *)key;
++ (void)removeCoordinateForKey:(NSString *)key;
 
 + (id)objectForKey:(NSString *)key;
 + (BOOL)boolForKey:(NSString *)key;
 + (NSInteger)integerForKey:(NSString *)key;
 + (CGFloat)floatForKey:(NSString *)key;
++ (NSURL *)URLForKey:(NSString *)key;
++ (CLLocationCoordinate2D)coordinateForKey:(NSString *)key;
 @end
--- HZExtensionKit/HZPreference/HZPreference.m
+++ HZExtensionKit/HZPreference/HZPreference.m
@@ -33,11 +33,31 @@
     [defaults setFloat:value forKey:key];
     [defaults synchronize];
 }
++ (void)saveURL:(NSURL *)URL forKey:(NSString *)key
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setURL:URL forKey:key];
+    [defaults synchronize];
+}
++ (void)saveCoordinate:(CLLocationCoordinate2D)coordinate forKey:(NSString *)key
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setFloat:coordinate.latitude forKey:[key stringByAppendingString:@".latitude"]];
+    [defaults setFloat:coordinate.longitude forKey:[key stringByAppendingString:@".longitude"]];
+    [defaults synchronize];
+}
 
 + (void)removeObjectForKey:(NSString *)key
 {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults removeObjectForKey:key];
+    [defaults synchronize];
+}
++ (void)removeCoordinateForKey:(NSString *)key
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults removeObjectForKey:[key stringByAppendingString:@".latitude"]];
+    [defaults removeObjectForKey:[key stringByAppendingString:@".longitude"]];
     [defaults synchronize];
 }
 
@@ -60,5 +80,18 @@
 {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     return [defaults floatForKey:key];
+}
++ (NSURL *)URLForKey:(NSString *)key
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    return [defaults URLForKey:key];
+}
++ (CLLocationCoordinate2D)coordinateForKey:(NSString *)key
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    return CLLocationCoordinate2DMake(
+                                      [defaults floatForKey:[key stringByAppendingString:@".latitude"]],
+                                      [defaults floatForKey:[key stringByAppendingString:@".longitude"]]
+                                      );
 }
 @end
